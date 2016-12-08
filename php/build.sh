c674096e687b2d58903a9d8b9208ba63ace61bd8
Chris Hilsdon <chris@koolserve.uk>
2016-12-08 00:49:43 +0000

    Use redis to store sessions

--- php/build.sh
+++ php/build.sh
@@ -1,24 +1,2 @@
-#!/bin/bash
-
-if [[ -z $1 ]]; then
-    echo "You need to pass a path to mount"
-    exit;
-fi;
-
-name='php'
-version='koolserve/php'
-path=$1
-
-if [[ $2 == "true" ]]; then
-    version='koolserve/php:devel'
-    docker build -t $version .
-fi;
-
-docker rm -f $name
-docker run -d \
-    --restart always \
-    --name $name -p 9000:9000 \
-    -v $path:/var/www/html \
-    --link percona:percona \
-    $version
-docker logs -f $name
+version='koolserve/php:devel'
+docker build -t $version .
